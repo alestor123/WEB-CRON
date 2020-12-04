0108366af98c2c2525c7809b39a5f89e4b299338
alestor123 <alestoraldous@gmail.com>
2020-12-04 21:45:03 +0530

    setting express api

--- App.js
+++ App.js
@@ -2,3 +2,5 @@ var express = require('express'),
 app = express(),
 port =  process.argv[2] || process.env.PORT || 3000 ,
 cron = require('node-cron');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
