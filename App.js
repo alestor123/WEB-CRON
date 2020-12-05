40f7f158822109cf516ed6505143beddfcad67b5
alestor123 <alestoraldous@gmail.com>
2020-12-05 16:59:34 +0530

    added cli

--- App.js
+++ App.js
@@ -3,14 +3,45 @@
 require('dotenv').config()
 var express = require('express'),
 app = express(),
-chalk = require('chalk')
-port =  process.argv[2] || process.env.PORT || 3000,
+chalk = require('chalk'),
+pck = require('./package.json'),
+argv = process.argv[2],  
+port =  argv || process.env.PORT || 3000,
 cron = require('node-cron'),
-key = process.env.KEY || process.argv[3],
+key = process.env.KEY || process.argv[3] || 'key',
 { exec } = require('child_process');
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+if(argv== '-v' ||argv == '--version'){
+    console.log( `${pck.version}`)
+  process.exit(1);
+}
+else if (argv =='-h'|| argv == '--help') { // checking undifined args
+    console.log(`
+    Usage: ${pck.name} <Port> <Key>
+`);
+}
+else if (argv =='-i'|| argv == '--issue') { // checking undifined args
+  console.log(`
+  Issues at ${pck.bugs.url} 
+`);
+}
+
+else if (argv =='-a'|| argv == '--author') { // checking undifined args
+  console.log(`
+  Author: ${pck.author} 
+`);
+}
+
+else if (argv =='-d'|| argv == '--docs') { // checking undifined args
+  console.log(`
+  Docs at ${pck.homepage} 
+`);}
+
+else{
+    app.listen(port, () => console.log(chalk.green(`Server running at ${port}`)))
+}
 app.get('/', (req, res) => {
     res.json({'info':'This is the cron server'})
 })
@@ -29,4 +60,3 @@ else{
     res.status(401).send('Auth Error')
     console.log(chalk.red(Date() + ':' + req.ip + ' Auth Error 401'))
 }})
-app.listen(port, () => console.log(chalk.green(`Server running at ${port}`)))
