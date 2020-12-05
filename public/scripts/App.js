a3b4e017eaa1b2c2a28f3944a86c9cd7603790a2
alestor123 <alestoraldous@gmail.com>
2020-12-05 14:21:58 +0530

    added alert and icon

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -9,13 +9,15 @@ var inputs = document.getElementsByName("quantity"),
         return input.value;
     }).join(' ');
 var finalCron =  cron.split('0').join('*');
-alert(finalCron)
-alert(`Cron:${cron} , Command:${ command.value}`)
-if (confirm("Are You Sure To Deploy")) {
+if(!confirm(`Cron:${cron} , Command:${ command.value}`)){
+    console.log('confirmed comman')
+}
+ else if (confirm("Are You Sure To Deploy")) {
 axios.post('/api/v1', {
 command: command.value,
 cron: finalCron
-})  
+})
+alert('Sucess')  
 } else {
 alert('Aborted')
 }
